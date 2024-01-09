9f6057b37c78718ec8f651df68a39ce92ad15d31
Loveleen Kaur <126374845+leenubhagtana@users.noreply.github.com>
2024-01-09 16:25:54 +0530

    Update subcategory.php

--- shopping/shopping/admin/subcategory.php
+++ shopping/shopping/admin/subcategory.php
@@ -11,15 +11,15 @@ if(isset($_POST['submit']))
 {
 	$category=$_POST['category'];
 	$subcat=$_POST['subcategory'];
-$sql=mysqli_query($con,"insert into subcategory(categoryid,subcategory) values('$category','$subcat')");
-$_SESSION['msg']="SubCategory Created !!";
+        $sql=mysqli_query($con,"insert into subcategory(categoryid,subcategory) values('$category','$subcat')");
+        $_SESSION['msg']="SubCategory Created !!";
 
 }
 
 if(isset($_GET['del']))
 		  {
 		          mysqli_query($con,"delete from subcategory where id = '".$_GET['id']."'");
-                  $_SESSION['delmsg']="SubCategory deleted !!";
+                          $_SESSION['delmsg']="SubCategory deleted !!";
 		  }
 
 ?>
